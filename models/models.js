97ac609963d6d61e3d6234647fd826e304c24efd
Matt-GitHub <matthewjamesbedard@outlook.com>
2020-03-19 03:16:56 -0400

    added ability to login and register, created token generator that provides all user information, modified models bug

--- models/models.js
+++ models/models.js
@@ -1,9 +1,45 @@
 const db = require("../db/db-config");
 
 module.exports = {
-  getAll
+  getAll,
+  register,
+  login,
+  deleteAccount
 };
 
-function getAll(database) {
+// GENERAL QUERIES
+
+function getAll(database, id) {
   return db(database);
+}
+
+function addData(database, data) {
+  return db(database).insert(data);
+}
+
+function removeData(database, itemId) {
+  return db(database)
+    .where(itemId)
+    .delete();
+}
+
+function editData(database, itemId) {
+  return db(database)
+    .where(itemId)
+    .delete();
+}
+
+// CREDENTIALS
+function register(info) {
+  return db("users").insert(info);
+}
+
+function login(credentials) {
+  return db("users").where(credentials);
+}
+
+function deleteAccount(credentials, id) {
+  return db("users")
+    .where(credentials, id)
+    .delete();
 }
--- routers/user-router.js
+++ routers/user-router.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const user = require("../models/models");
+const bcrypt = require("bcryptjs");
+const tokenGenerator = require("../token/token-generator");
 const helmet = require("helmet");
 const router = express();
 
@@ -14,6 +16,40 @@ router.get("/users", (req, res) => {
     .catch(err => {
       console.log("error", err);
       res.status(500).json({ errorMessage: "cannot get users at this time" });
+    });
+});
+
+router.post("/register", (req, res) => {
+  const credentials = req.body;
+  const hash = bcrypt.hashSync(credentials.password, 12);
+  credentials.password = hash;
+  user
+    .register(credentials)
+    .then(newUser => {
+      res.status(201).json({ message: "Welcome, thank you for joining!" });
+    })
+    .catch(err => {
+      console.log("error", err);
+      res.status(500).json({ errorMessage: "Error regestering account :(" });
+    });
+});
+
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  user
+    .login({ email })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenGenerator(user);
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({ message: `Invalid credentials` });
+      }
+    })
+    .catch(err => {
+      console.log("login error", err);
+      res.status(500).json({ errorMessage: `Error logging in` });
     });
 });
 
